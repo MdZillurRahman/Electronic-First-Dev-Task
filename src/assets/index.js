7df719c0518e973ab8ea2e9ed9936a7146242ea2
MdZillurRahman <mzisanr@gmail.com>
2023-08-20 06:42:37 +0600

    Responsive nav menu + Search Implemented

---
+++ src/assets/index.js
@@ -0,0 +1,67 @@
+let navList = document.querySelector('.nav-list.lower-part');
+
+function searchIcon() {
+  if (window.innerWidth <= 799) {
+    document.querySelector('.searchField.mobile-search').classList.add('show');
+    document.querySelector('#searchFieldMobile').focus();
+  } else {
+    document.querySelector('.searchField').classList.add('show');
+    document.querySelector('#searchField').focus();
+  }
+
+  if (document.querySelector('.nav-list.active')) {
+    document.querySelector('.nav-list').classList.remove('active');
+  }
+}
+
+navList.addEventListener('click', (e) => {
+  let elementCapture = e.target.parentElement.querySelector('.nav-dropdown');
+  let dropdownList = document.querySelectorAll('.show');
+
+  console.log(elementCapture);
+
+  if (
+    e.target.classList.contains('searchIcon') ||
+    e.target.classList.contains('fa')
+  ) {
+    searchIcon();
+  } else if (e.target.classList.contains('crossButton')) {
+    document
+      .querySelector('.nav-dropdown.searchField')
+      .classList.remove('show');
+  } else {
+    elementCapture.classList.toggle('show');
+  }
+  dropdownList.forEach((item) => {
+    item.classList.remove('show');
+  });
+});
+
+document.querySelector('p.searchIcon').addEventListener('click', (e) => {
+  if (
+    e.target.classList.contains('searchIcon') ||
+    e.target.classList.contains('fa')
+  ) {
+    searchIcon();
+    document.querySelector('#nav-toggle').classList.remove('active');
+  }
+});
+
+document.querySelector('#nav-toggle').addEventListener('click', (e) => {
+  e.target.parentElement.classList.toggle('active');
+  document.querySelector('.nav-list').classList.toggle('active');
+  if (document.querySelector('.searchField.mobile-search.show')) {
+    document
+      .querySelector('.searchField.mobile-search')
+      .classList.remove('show');
+  }
+});
+
+document
+  .querySelector('.mobile-search .crossButton')
+  .addEventListener('click', (e) => {
+    document
+      .querySelector('.nav-dropdown.searchField.mobile-search')
+      .classList.remove('show');
+  });
+
